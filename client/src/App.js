d8c8062106c778f1ff8ce85f712b2333a67eb763
antoinelavigne04 <alavi091@uottawa.ca>
2022-06-02 08:32:10 -0400

    More testing :)

--- client/src/App.js
+++ client/src/App.js
@@ -12,13 +12,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-        <p>Testing pipeline v2</p>
-      </header>
-    </div>
+      <p>Yo that it is cool!!!</p>
   );
 }
 
